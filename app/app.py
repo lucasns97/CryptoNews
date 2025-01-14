052495b53c263b00f3b9fbd56f2d4f02321b08bb
lucasnseq <43300925+lucasns97@users.noreply.github.com>
2025-01-14 12:16:22 -0300

    Add crypto_name parameter that was missing

--- app/app.py
+++ app/app.py
@@ -9,16 +9,26 @@ def lambda_handler(event: dict, context: dict) -> dict:
     try:
         # Fetch news articles
         today = datetime.now()
-        news_articles = fetch_articles(CONFIG.get("CRYPTO_NAME"), date=today, top_k=10)
+        news_articles = fetch_articles(
+            query=CONFIG.get("CRYPTO_NAME"),
+            date=today,
+            top_k=1
+        )
         if not news_articles:
             return {"statusCode": 200, "body": "No news articles available."}
 
         # Analyze news articles
-        analysis = analyze_news_with_llm(news_articles)
+        analysis = analyze_news_with_llm(
+            news_articles=news_articles,
+            crypto_name=CONFIG.get("CRYPTO_NAME")
+        )
 
         # Send email alert if a market drop is detected
         if analysis.get("ValueWillDrop", False):
-            send_email_alert(analysis.get("Reasoning", "No reasoning provided."))
+            send_email_alert(
+                justification=analysis.get("Reasoning", "No reasoning provided."),
+                crypto_name=CONFIG.get("CRYPTO_NAME")
+            )
             return {"statusCode": 200, "body": "Alert email sent.", "analysis": analysis}
         else:
             return {"statusCode": 200, "body": "No market drop detected.", "analysis": analysis}
